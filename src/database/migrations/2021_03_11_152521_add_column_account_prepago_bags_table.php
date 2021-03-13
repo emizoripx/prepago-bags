550409c3b2eea5684488d00153d8be85964bc543
Marco Cuellar <mcuellar@ipxserver.com>
2021-03-12 20:55:39 -0400

    rename migration

--- src/database/migrations/2021_03_11_152521_add_column_account_prepago_bags_table.php
+++ src/database/migrations/2021_03_11_152521_add_column_account_prepago_bags_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnAccountPrepagoBagsTable extends Migration
+class AddColumnAccountPrepagoBags1Table extends Migration
 {
     /**
      * Run the migrations.
