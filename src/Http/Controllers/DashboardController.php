e580cd20355fc8143c8e183fbb40142a70c72bff
Marco Cuellar <mcuellar@ipxserver.com>
2021-03-18 21:19:39 -0400

    - action for pass from pilot testing - action for pass to production - list changed with data from user

--- src/Http/Controllers/DashboardController.php
+++ src/Http/Controllers/DashboardController.php
@@ -10,17 +10,47 @@ class DashboardController extends Controller
     public function clientsList(){
         
      
-        $clientsPrepago = AccountPrepagoBags::rightJoin('companies', 'account_prepago_bags.company_id', '=', 'companies.id')
-                                            ->leftJoin('users','users.account_id','=','companies.account_id')
-                                            // ->leftJoin('company_user', 'company_user.user_id','=','users.id')
-                    ->select('account_prepago_bags.id','account_prepago_bags.company_id' ,'companies.settings' ,'account_prepago_bags.production', 'account_prepago_bags.is_postpago', 'account_prepago_bags.enabled', 'account_prepago_bags.phase')
+        $clientsPrepago = AccountPrepagoBags::join('company_user', 'company_user.company_id','=','account_prepago_bags.company_id')
+                                            ->join('companies', 'account_prepago_bags.company_id', '=', 'companies.id')
+                                            ->Join('users','users.id','=','company_user.user_id')
+                    ->select('companies.id','account_prepago_bags.company_id' ,'companies.settings' ,'account_prepago_bags.invoice_number_available','account_prepago_bags.production', 'account_prepago_bags.is_postpago', 'account_prepago_bags.enabled', 'account_prepago_bags.phase','users.email')
                     ->where('account_prepago_bags.delete', '=', false)
-                    // ->where('company_user.is_owner', 1)
+                    ->where('company_user.is_owner', 1)
+                    ->orderBy('companies.id','desc')
                     ->simplePaginate(30);
 
 
         return view('prepagobags::ListClients', compact('clientsPrepago'));
         // return $clientsPrepago;
 
+    }
+
+    public function showForm($companyid)
+    {
+        $data = AccountPrepagoBags::join('companies', 'account_prepago_bags.company_id', '=', 'companies.id')
+                            ->select(
+                                'companies.id', 
+                                'account_prepago_bags.company_id', 
+                                'companies.settings'
+                                )
+                            ->where('account_prepago_bags.delete', '=', false)
+                            ->where('companies.id',$companyid)
+                            ->first();
+
+        return view('prepagobags::components.modal',["company" => $data]);
+    }
+    public function showForm2($companyid)
+    {
+        $data = AccountPrepagoBags::join('companies', 'account_prepago_bags.company_id', '=', 'companies.id')
+                            ->select(
+                                'companies.id', 
+                                'account_prepago_bags.company_id', 
+                                'companies.settings'
+                                )
+                            ->where('account_prepago_bags.delete', '=', false)
+                            ->where('companies.id',$companyid)
+                            ->first();
+
+        return view('prepagobags::components.modal2',["company" => $data]);
     }
 }
--- src/Resource/views/ListClients.blade.php
+++ src/Resource/views/ListClients.blade.php
@@ -6,105 +6,142 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.9.0/jquery.validate.min.js"></script>
     <title>Clientes</title>
 </head>
 
 <body>
     <div class="container">
-        @php
-        $company = (object)["business_name" => "mi compania", "nit" => "2342342342342"];
-        @endphp
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Pasar a pruebas piloto</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <form>
-                            
-                            <div class="form-group ">
-                                <div class="form-group">
-                                Razón Social : {{ $company->business_name }} <br />
-                                NIT : {{ $company->nit }} <br />
-                                </div>
+        <h3>Clientes de la plataforma</h3>
+        <div >
+            <table class="table table-striped table-bordered" style="table-layout: fixed;">
+                <thead>
+                    <th width="5%">#</th>
+                    <th width="10%">Compañia</th>
+                    <th width="20%">Dueño</th>
+                    <th width="15%">Cant disponible facturas</th>
+                    <th width="10%">Producción</th>
+                    <th width="10%">Tipo de Cuenta</th>
+                    <th width="10%">Fase</th>
+                    <th width="10%">Estado</th>
+                    <th width="10%">Acciones</th>
+                </thead>
+                <tbody>
+                    @foreach($clientsPrepago as $client)
+                    <tr>
+                        <td>{{ $client->id }}</td>
+                        <td>{{ json_decode($client->settings)->name }}</td>
+                        <td>{{ $client->email }}</td>
+                        <td>{{ $client->invoice_number_available }}</td>
 
+                        <td align="center">{{ $client->production ? "Sí" : "No" }}</td>
+                        <td align="center">{!! $client->account_type_badge !!}</td>
+                        <td align="center">{!! $client->phase !!}</td>
+                        <td align="center">{{ $client->enabled == 'active' ? "Activo": "Suspendido" }}</td>
+                        <td align="center">
+                            <div class="dropdown">
+                                <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Acciones
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    @if ($client->phase == "Testing")
+                                    <a class="dropdown-item" data-type="actions-dashboard" data-href="{{ URL::to('dashboard/form-phase-piloto/' . $client->company_id)}}" data-container=".action-modal">Pasar a pruebas piloto</a>
+                                    <a class="dropdown-item" data-type="actions-dashboard" data-href="{{ URL::to('dashboard/form-phase-production/' . $client->company_id)}}" data-container=".action-modal">Pasar a producción</a>
+                                    @elseif( $client->phase == 'Piloto testing')
+                                    <a class="dropdown-item" data-type="actions-dashboard" data-href="{{ URL::to('dashboard/form-phase-production/' . $client->company_id)}}" data-container=".action-modal">Pasar a producción</a>
+
+                                    @endif
+
+
+                                    @if($client->is_postpago )
+                                    @if($client->enabled == 'active')
+                                    <a class="dropdown-item disabled" href="#">Suspender</a>
+                                    @else
+                                    <a class="dropdown-item disabled" data-type="data-action" data- onclick="enablePostpago( $client->company_id )">Habilitar</a>
+                                    @endif
+                                    @endif
+                                </div>
                             </div>
-                            <div class="form-group">
-                                <label for="client_id">Client ID</label>
-                                <input type="text" class="form-control" id="client_id" placeholder="Client ID" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="client_secret">Client Secret</label>
-                                <input type="text" class="form-control" id="client_secret" placeholder="Client Secret" required>
-                            </div>
-                                             
-                        </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
-                        <button type="button" class="btn btn-success">Ejecutar</button>
-                    </div>
-                </div>
-            </div>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>  
+            {{ $clientsPrepago->links() }}
+
+
         </div>
 
 
-        <h3>Clientes</h3>
-        <table class="table table-striped table-bordered" style="table-layout: fixed;">
-            <thead>
-                <th width="50%">Compañia</th>
-                <th width="10%">Producción</th>
-                <th width="10%">Tipo de Cuenta</th>
-                <th width="10%">Fase</th>
-                <th width="10%">Estado</th>
-                <th width="20%">Acciones</th>
-            </thead>
-            <tbody>
-                @foreach($clientsPrepago as $client)
-                <tr>
-                    <td>{{ json_decode($client->settings)->name }}</td>
-                    
-                    <td align="center">{{ $client->production ? "Sí" : "No" }}</td>
-                    <td align="center">{!! $client->account_type_badge !!}</td>
-                    <td align="center">{!! $client->phase !!}</td>
-                    <td align="center">{{ $client->enabled == 'active' ? "Activo": "Suspendido" }}</td>
-                    <td align="center">
-                        <div class="dropdown">
-                            <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Acciones
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                @if($client->phase != "Production")
-                                <a class="dropdown-item" data-toggle="modal" data-target="#exampleModal">Pasar a pruebas piloto</a>
-                                @else
-                                <a class="dropdown-item disabled">Pasar a pruebas piloto</a>
-                                @endif
-                                <a class="dropdown-item" href="#">Pasar a produccion</a>
-
-                                @if($client->is_postpago )
-                                @if($client->enabled == 'active')
-                                <a class="dropdown-item disabled" href="#">Suspender</a>
-                                @else
-                                <a class="dropdown-item disabled" href="#">Habilitar</a>
-                                @endif
-                                @endif
-                            </div>
-                        </div>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>  
-        {{ $clientsPrepago->links() }}
     </div>
+
+    <div class="modal fade action-modal" tabindex="-1" role="dialog" aria-labelledby="gridSystemModalLabel"></div>
+
+
+
+    <script>
+        $(document).on('click', '[data-type=actions-dashboard]', function() {
+            jQuery.get($(this).attr('data-href'), function(res) {
+                $(".action-modal").html(res);
+                $(".action-modal").modal("show");
+            });
+        });
+        //On display of add contact modal
+        $('.action-modal').on('show.bs.modal', function(e) {
+
+            var modal = $(this);
+
+            $('form#actionFormLabel')
+                .submit(function(e) {
+                    e.preventDefault();
+                })
+                .validate({
+                    rules: {
+                        client_id: {
+                            required: true
+                        },
+                        client_secret: {
+                            required: true
+                        }
+                    },
+                    messages: {
+                        client_id: {
+                            required: "El client Id es requerido",
+                        },
+                        client_secret: {
+                            required: "El client secret es requerido"
+                        }
+                    },
+                    submitHandler: function(form) {
+                        e.preventDefault();
+                        console.log("submit en este formulario");
+                        var data = $(form).serialize();
+                        $(form)
+                            .find('button[type="submit"]')
+                            .attr('disabled', true);
+                        $.ajax({
+                            method: 'POST',
+                            url: $(form).attr('action'),
+                            dataType: 'json',
+                            data: data,
+                            success: function(result) {
+                                if (result.success == true) {
+                                    $('div.action-modal').modal('hide');
+                                    // alert("Se paso a pruebas piloto exitosamente");
+                                    location.reload();
+                                } else {
+                                    alert(result.msg);
+                                }
+                            },
+                        });
+                    },
+                });
+        });
+    </script>
+
 </body>
 
 </html>
