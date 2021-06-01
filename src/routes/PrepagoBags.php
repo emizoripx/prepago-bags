1dcaafb184581020691b89b2e9c6dabd58dbb86d
alvaro-ipx <remberto.molina@ipxserver.com>
2021-06-01 15:12:06 -0400

    Added fel_pines

--- src/routes/PrepagoBags.php
+++ src/routes/PrepagoBags.php
@@ -2,7 +2,9 @@
 
 namespace EmizorIpx\PrepagoBags\routes;
 
+use EmizorIpx\PrepagoBags\Models\FelPin;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
 class PrepagoBags {
 
@@ -37,7 +39,30 @@ class PrepagoBags {
             Route::post('dashboard/production-up', 'CompanyAccountController@productionUp')->name('dashboard.production');
             Route::get('dashboard/form-phase-piloto/{company_id}', 'DashboardController@showForm');
             Route::get('dashboard/form-phase-production/{company_id}', 'DashboardController@showForm2');
-        });
+            Route::get('getUuid', function () {
+                $array = [];
+
+                
+                    for ($i = 0; $i < 200; $i++) {
+                        // $string = strtoupper(Str::uuid()->toString());
+                        do {
+                            usleep(200000);
+                            $newPin = rand(1111,9999)."-".time()."-".rand(1111,9999);
+                        } while (FelPin::where('pin', $newPin)->exists());
+                        \Log::debug($newPin);
+                         echo $newPin. '  -';
+                        if (in_array($newPin, $array)) {
+                             echo ' True ';
+                        } else {
+                             echo ' False ';
+                        }
+                        $array [] = $newPin;
+                        
+                    }
+                
+                
+                });
+            });
 
     }
 }
