7489b71e2d93a4756a15b55f8b4f0e1cb3229698
alvaro-ipx <remberto.molina@ipxserver.com>
2021-06-01 17:09:41 -0400

    Fixed

--- src/routes/PrepagoBags.php
+++ src/routes/PrepagoBags.php
@@ -39,30 +39,8 @@ class PrepagoBags {
             Route::post('dashboard/production-up', 'CompanyAccountController@productionUp')->name('dashboard.production');
             Route::get('dashboard/form-phase-piloto/{company_id}', 'DashboardController@showForm');
             Route::get('dashboard/form-phase-production/{company_id}', 'DashboardController@showForm2');
-            Route::get('getUuid', function () {
-                $array = [];
-
-                
-                    for ($i = 0; $i < 200; $i++) {
-                        // $string = strtoupper(Str::uuid()->toString());
-                        do {
-                            usleep(200000);
-                            $newPin = rand(1111,9999)."-".time()."-".rand(1111,9999);
-                        } while (FelPin::where('pin', $newPin)->exists());
-                        \Log::debug($newPin);
-                         echo $newPin. '  -';
-                        if (in_array($newPin, $array)) {
-                             echo ' True ';
-                        } else {
-                             echo ' False ';
-                        }
-                        $array [] = $newPin;
-                        
-                    }
-                
-                
-                });
-            });
+            
+        });
 
     }
 }
