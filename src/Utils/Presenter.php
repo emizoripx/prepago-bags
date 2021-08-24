2ce582bfe609ee4859e1aeea4efff14584b14b93
alvaro-ipx <remberto.molina@ipxserver.com>
2021-08-24 14:42:56 -0400

    Add plan postpago to fel_data

--- src/Utils/Presenter.php
+++ src/Utils/Presenter.php
@@ -4,11 +4,13 @@
 namespace EmizorIpx\PrepagoBags\Utils;
 
 use EmizorIpx\PrepagoBags\Http\Resources\AccountDetailResource;
+use EmizorIpx\PrepagoBags\Http\Resources\PostpagoPlanCompanyResource;
 use EmizorIpx\PrepagoBags\Http\Resources\PrepagoBagResource;
 use EmizorIpx\PrepagoBags\Http\Resources\PurchaseHistoryResource;
 use EmizorIpx\PrepagoBags\Models\AccountPrepagoBags;
 use EmizorIpx\PrepagoBags\Models\PrepagoBag;
 use EmizorIpx\PrepagoBags\Models\PrepagoBagsPurchaseHistorial;
+use EmizorIpx\PrepagoBags\Services\PostpagoPlanCompanyService;
 use Illuminate\Support\Facades\Log;
 
 class Presenter {
@@ -16,6 +18,7 @@ class Presenter {
 
     public static function appendBagsFelData($data, $company_id){
         $data['fel_data']['bags'] = PrepagoBagResource::collection( PrepagoBag::getAllBags($company_id) );
+        $data['fel_data']['plan_postpago'] = new PostpagoPlanCompanyResource( PostpagoPlanCompanyService::getPostpagoPlan($company_id) );
         // $data['fel_data']['company_detail'] = new AccountDetailResource(AccountPrepagoBags::getCompanyDetail($company_id));
         $data['fel_data']['purchase_history'] = PurchaseHistoryResource::collection(PrepagoBagsPurchaseHistorial::getPurchaseHistorial($company_id));
         return $data;
