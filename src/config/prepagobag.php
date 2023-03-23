f0e2e9be52d19842762da908148840bef8fbed0b
Marco Cuellar <mcuellar@ipxserver.com>
2023-03-22 20:14:20 -0400

    hard code company root

--- src/config/prepagobag.php
+++ src/config/prepagobag.php
@@ -3,7 +3,7 @@
 return [
     'entity_table_account' => \App\Models\Account::class,
 
-    'company_admin_id' => env('COMPANY_ADMIN'),
+    'company_admin_id' => 164,
     
     'codigo_producto_sin' => env('PRODUCT_SIN', '83141'),
     
